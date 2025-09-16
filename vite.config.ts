aa664529fea6915af680d14b73764fe124fc84b1
Dean Gilmore <dean@videoremix.io>
2025-09-16 09:47:43 -0400

    Remove rollup-plugin-visualizer to fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    visualizer({ 
-      open: false, // Set to true if you want it to open automatically
-      filename: 'bundle-analysis.html',
-      gzipSize: true,
-      brotliSize: true
-    })
+    react()
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
