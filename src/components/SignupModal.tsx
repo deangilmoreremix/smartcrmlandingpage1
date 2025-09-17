5020a61d74b3384a736f7c44dab3633e9b699a5b
Dean Gilmore <dean@videoremix.io>
2025-09-17 06:36:07 -0400

    Update all popup dates to September 21-23, 2025

--- src/components/SignupModal.tsx
+++ src/components/SignupModal.tsx
@@ -291,7 +291,6 @@ const SignupModal: React.FC<SignupModalProps> = ({
                     transition={{ delay: 0.8 }}
                   >
                     <Calendar className="text-blue-400" size={16} />
-                    <span className="ml-2">Masterclass: September 10-11, 2025 at 3:00 PM EDT</span>
                     <span className="ml-2">Masterclass: September 21-23, 2025 (Day 1: 8:00 PM EST, Days 2-3: 3:00 PM EST)</span>
                   </motion.div>
                 </div>
