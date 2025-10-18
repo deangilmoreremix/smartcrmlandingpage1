0219cb399ccd663b78d779aeac2a1a0d70ac5eb8
Dean Gilmore <dean@videoremix.io>
2025-10-18 06:23:02 -0400

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero: React.FC<HeroProps> = ({ title, subtitle, launchDate }) => {
   };
 
   return (
-    <div className="relative min-h-screen flex flex-col items-center justify-center px-4 py-12 overflow-hidden mt-[108px]">
+    <div className="relative min-h-screen flex flex-col items-center justify-center px-4 py-12 overflow-hidden mt-[310px]">
       <AnimatedIconsGroup
         section="hero"
         iconCount={8}
